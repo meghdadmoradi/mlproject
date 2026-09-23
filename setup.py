33a1b112a2d2c78ba4da7d81ae3501812c9bc377
meghdadmoradi <mmoradimeghdad@gmail.com>
2026-09-23 16:04:36 +0330

    setup

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='krish',
-author_email='krishnaik06@gmail.com',
+author='meghdad',
+author_email='mmoradimeghdad@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
